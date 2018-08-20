97f381f2ebe805010b2496302fb6e22c40bfcc75
Amit Rawat <georgianamitrawat@gmail.com>
2018-08-20 23:19:28 +0530

    made a the hello world program in c++

---
+++ hello-world/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // prints hello world on separate lines
+    cout << "hello" << endl << "world";
+    cout << endl ;
+    cout << "hello\nworld";
+    cout << endl;
+    // prints hello world on the same line
+    cout << "hello world";    // passing as single argument
+    cout << endl;
+    cout << "hello " << "world";  // passing as two arguments
+    // prints hello world on the same line
+
+}
