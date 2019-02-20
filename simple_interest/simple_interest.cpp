98d4e408fb47ebbc26b550bd7bc81419422e9dbd
unknown <Student@B1LIBPC5.cuchdit.in>
2019-02-20 13:22:04 +0530

    created simple interest program

---
+++ simple_interest/simple_interest.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float p;
+	float r;
+	float t;
+	// Taking an input for principle
+	cout << "Enter the principle: ";
+	cin >> p;
+	// Taking an input for rate per year
+	cout << "Enter the rate: ";
+	cin >> r;
+	// Taking an input for time
+	cout << "Enter the time(In Year): ";
+	cin >> t;
+	// formula for S.I. is (p*r*t)/100
+	float si = (p*r*t)/100;
+	
+	// printing the simple interest
+	cout << "The Simple Interest is "  << si;
+
+    return 0;
+}//
