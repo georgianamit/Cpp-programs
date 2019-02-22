fc1e4fcf37ac54dee43464bddb8f2babdca23480
Amit Rawat <spider@localhost.localdomain>
2019-02-22 23:22:27 +0530

    completed simple interest

--- simple_interest/simple_interest.cpp
+++ simple_interest/simple_interest.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
@@ -28,7 +20,7 @@ int main()
 	float si = (p*r*t)/100;
 	
 	// printing the simple interest
-	cout << "The Simple Interest is "  << si;
+	cout << "The Simple Interest is "  << si << endl;
 
     return 0;
 }
