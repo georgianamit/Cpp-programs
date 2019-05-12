91b949cb40c44ccd465492099e826307680a1ad3
Amit Rawat <georgianamitrawat@gmail.com>
2019-05-12 20:58:13 +0530

    completed the reverse number programs

--- adding_two_numbers/main.cpp
+++ adding_two_numbers/main.cpp
@@ -3,8 +3,9 @@
 using namespace std;
  
 int main()
-{
-    int a, b, sum;
+{   
+    // int a, b, sum;
+    float a, b, sum;
 
     cout << "Enter first number: ";
     cin >> a;
---
+++ reverse_number_recursion/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+ 
+using namespace std;
+ 
+
+int reverseNumber(int num);
+ 
+int main()
+{
+    int num, reverse;
+ 
+    cout<<"Enter a number: ";
+    cin>>num;
+ 
+    reverse = reverseNumber(num);
+ 
+    cout<<"Reverse Number: "<<reverse << endl;
+ 
+    return 0;
+}
+ 
+//Recursive function to find reverse of any number 
+int reverseNumber(int num)
+{
+    // Finding total digits in num
+    int digits = (int) log10(num);
+ 
+    // Base condition
+    if(num == 0)
+        return 0;
+ 
+    return ((num%10 * pow(10, digits)) + reverseNumber(num/10));
+}
