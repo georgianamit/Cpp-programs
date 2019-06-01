2a07672853c8d96b98e60ec91107f1fa80dcd0ae
Amit Rawat <georgianamitrawat@gmail.com>
2019-06-01 11:06:44 +0530

    corrected the prime number program

--- prime_number/main.cpp
+++ prime_number/main.cpp
@@ -11,19 +11,19 @@ int main(){
 
     if(num <= 1){
         isPrime = false;
-    }
-
-    for(int i = 2; i <= num/2; i++){  
-        if(num % i == 0){  
-            isPrime = false;  
-            break;  
+    }else{
+        for(int i = 2; i <= num/2; i++){  
+            if(num % i == 0){  
+                isPrime = false;  
+                break;  
+            }  
         }  
-    }  
+    }
 
     if (isPrime)  
         cout << num << " is Prime." << endl;  
     else
-        cout<< num << " is not Prime." << endl;  
+        cout<< num << " is not Prime." << endl; 
 
     return 0;  
 }  
--- prime_number_sqrt/main.cpp
+++ prime_number_sqrt/main.cpp
@@ -12,14 +12,14 @@ int main(){
 
     if(num <= 1){
         isPrime = false;
+    }else{
+        for(int i = 2; i <= sqrt(num); i++){  
+            if(num % i == 0){  
+                isPrime = false;  
+                break;  
+            }  
+        }
     }
-
-    for(int i = 2; i <= sqrt(num); i++){  
-        if(num % i == 0){  
-            isPrime = false;  
-            break;  
-        }  
-    }  
 
     if (isPrime)  
         cout << num << " is Prime." << endl;  
